015edccef8da4094b2b6cf236a165e03f589c109
Graeme Spence <graeme@simplemachines.org>
2012-08-05 14:40:16 +0100

    Send to the database

--- createuser.php
+++ createuser.php
@@ -6,6 +6,9 @@ include "settings.php";
 include "header.php";
 
 $createtrue = $_POST['createtrue'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 echo"
 <form action='createuser.php' method='post'>
@@ -19,12 +22,12 @@ Password: <input type='password' name='password' /><br />
 
 if(isset($createtrue)){
 
-/*
+
 mysql_connect($dbhost,$dbuser,$dbpass);
 
 @mysql_select_db($dbname) or die( "Unable to select database");
 
-$query = "INSERT INTO visits VALUES ('','$managerid','$dayte','$typeid','$friendly1','$friendly2','$friendly3','$friendly4','$accuracy1','$accuracy2','$accuracy3','$speed1','$quality1','$quality2','$quality3','$cleanliness1','$cleanliness2','$cleanliness3','$cleanliness4','$cleanliness5','$cleanliness6','$storeid','$totaladd','$grandtotal','$completeby')";
+$query = "INSERT INTO users VALUES ('','$name','$email','$password')";
 
 mysql_query($query);
 
@@ -32,7 +35,7 @@ $lastid = mysql_insert_id();
 
 mysql_close();
 
-*/
+
 echo"$createtrue";
 }
 
